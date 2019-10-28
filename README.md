bca10817510d6fe72cfb1f3aad6bebf450b65014
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2019-10-28 12:22:57 +0000

    info files

---
+++ README.md
@@ -0,0 +1,2 @@
+# BioDare2-ExcelTools
+Higher level abstraction on top of Apache POI for accessing data tables from Excel files
--- src/main/java/ed/synthsys/util/excel/Excel2TextConverter.java
+++ src/main/java/ed/synthsys/util/excel/Excel2TextConverter.java
@@ -16,29 +16,28 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
  * @author Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
  */
 public class Excel2TextConverter {
-    
+
     final XLSX2TextConverter xlsxConverter = new XLSX2TextConverter();
     final Workbook2TextConverter workbookConverter = new Workbook2TextConverter();
-    
-    int WORKBOOK_SIZE_THRESHOLD = 25*1024*1024; // 25Mb
-    
+
+    int WORKBOOK_SIZE_THRESHOLD = 25 * 1024 * 1024; // 25Mb
+
     public void convert(Path inFile, Path outFile) throws IOException, InvalidFormatException {
         convert(inFile, outFile, ",");
     }
-    
+
     public void convert(Path inFile, Path outFile, String SEP) throws IOException, InvalidFormatException {
-        
-        if (Files.size(inFile) <  WORKBOOK_SIZE_THRESHOLD) {
+
+        if (Files.size(inFile) < WORKBOOK_SIZE_THRESHOLD) {
             workbookConverter.convert(inFile, outFile, SEP);
         } else {
             if (isOldXLS(inFile)) {
                 throw new IOException("Cannot convert old xls files larger than "
-                        +WORKBOOK_SIZE_THRESHOLD+" please use xlsx format instead");
+                        + WORKBOOK_SIZE_THRESHOLD + " please use xlsx format instead");
             } else {
                 xlsxConverter.convert(inFile, outFile, SEP);
             }
         }
     }
 
-    
 }
